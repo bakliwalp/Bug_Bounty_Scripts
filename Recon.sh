7441a6a64a480b672cff82cf905948b22eabfcf2
bakliwalp <54543994+bakliwalp@users.noreply.github.com>
2022-08-17 16:04:42 +0530

    Update Recon.sh

--- Recon.sh
+++ Recon.sh
@@ -8,6 +8,7 @@ info_path=$domain/info
 subdomain_path=$domain/subdomain
 screenshot_path=$domain/screenshots
 directory_enum=$domain/directory_enum
+gau=$domain/gau
 
 
 if [ ! -d "$domain" ];then
@@ -26,6 +27,9 @@ fi
 if [ ! -d "$directory_enum" ];then
         mkdir $directory_enum
 fi
+if [ ! -d "$gau" ];then
+        mkdir $gau
+fi
 
 echo -e "${RED} [+] Checking who it is ... ${RESET}"
 whois $1 > $info_path/whois.txt
@@ -41,26 +45,28 @@ amass enum -d $domain >> $subdomain_path/found.txt
 
 
 echo -e "${RED} [+] Checking What's Alive... ${RESET}"
-cat $subdomain_path/found.txt | grep $domain | sort -u | httprobe -prefer-https | grep https | sed 's/https\?:\/\///' | tee -a $subdomain_path/alive.txt
+cat $subdomain_path/found.txt | grep $domain | sort -u | httpx  | grep https | sed 's/https\?:\/\///' | tee -a $subdomain_path/alive.txt
+
+cat $subdomain_path/found.txt | grep $domain | sort -u | httpx -title -tech-detect -status-code >> $subdomain_path/techdetect.txt
 
 echo -e "${RED} [+] Taking the Screenshots... ${RESET}"
 gowitness file -f $subdomain_path/alive.txt -P $screenshot_path/ --no-http
 
 echo -e "${RED} [+] Starting Directory Enumneration using Gobuster... ${RESET}"
 
-for i in `cat $subdomain_path/alive.txt`
-do
-    gobuster dir -u https://$i -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt --discover-backup >> $directory_enum/Dir_enum.txt
-done
+#for i in `cat $subdomain_path/alive.txt`
+#do
+#   gobuster dir -u https://$i -w /usr/share/wordlists/dirb/big.txt >> $directory_enum/Dir_enum.txt
+#done
 
 echo -e "${RED} [+]Doing FFUF subdomain enum...${RESET}"
 
 echo -e "${RED} [+] This may take some time... Make sure you take a break and have a walk....${RESET}"
 
-for i in `cat $subdomain_path/alive.txt`
-do
-        ffuf -u https://$i/FUZZ -w /usr/share/wordlists/amass/all.txt -recursion >> $directory_enum/ffuf_enum.txt
-done
+#for i in `cat $subdomain_path/alive.txt`
+#do
+#       ffuf -u https://$i/FUZZ -w /usr/share/wordlists/amass/all.txt -c  >> $directory_enum/ffuf_enum.txt
+#done
 
 echo -e "${RED} [+] Getting all urls and checking probable XSS....${RESET}"
 echo "$1" | gau | kxss >> $gau/gau_xss.txt
